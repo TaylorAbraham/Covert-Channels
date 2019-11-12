3d5e5020c2144b88ce1f4982f8e3f97ccc5bdfa5
Ryan Abraham <Ryan_JA@live.ca>
2019-11-12 17:34:12 -0500

    Add client

---
+++ go_covert_lib/client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <button type="button" className="btn btn-primary">Make Covert Channel</button>
+    </div>
+  );
+}
+
+export default App;
