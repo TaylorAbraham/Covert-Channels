2261f03c04d40251c331185ac8350389882b2ffe
Ryan Abraham <Ryan_JA@live.ca>
2019-11-21 23:09:39 -0500

    [CC-14] Added first components for frontend

--- go_covert_lib/client/src/App.tsx
+++ go_covert_lib/client/src/App.tsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
+/**
+ * IMPORTANT NOTE: For styling, refer to https://getbootstrap.com/docs/4.0/utilities
+ */
 
 const App: React.FC = () => {
+  const [textToSend, setTextToSend] = useState("");
+
   return (
-    <div className="App">
-      <button type="button" className="btn btn-primary">Make Covert Channel</button>
+    <div className="App m-2">
+      <textarea
+        className="form-control w-25 m-1"
+        value={textToSend}
+        onChange={e => setTextToSend(e.target.value)}
+      />
+      <button type="button" className="btn btn-primary m-1">Send Message</button>
+      <br />
+      <div className="m-1">Incoming Messages</div>
+      <textarea
+        className="form-control w-25 m-1"
+        readOnly
+      />
+      <h2 className="m-1">Configuration</h2>
+      <button type="button" className="btn btn-primary m-1">Open Covert Channel</button>
+      <button type="button" className="btn btn-primary m-1">Close Covert Channel</button>
     </div>
   );
 }
