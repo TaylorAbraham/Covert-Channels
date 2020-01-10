5887baa4b6457befdc3cfa0040e4acbaf7b10488
fahid-mannan <fahidmannan@cmail.carleton.ca>
2020-01-09 22:46:25 -0500

    added padding + ability to select mode for aes

--- go_covert_lib/controller/processor/advancedEncryptionStandard/advancedEncryptionStandard.go
+++ go_covert_lib/controller/processor/advancedEncryptionStandard/advancedEncryptionStandard.go
@@ -4,22 +4,42 @@ import (
 	"crypto/aes"
 	"crypto/cipher"
 	"errors"
+	"fmt"
 )
 
+const padIndex = 7;
+
 type AdvancedEncryptionStandard struct {
+	mode string
 	key []byte
 }
 
 func(c *AdvancedEncryptionStandard) Process(data []byte) ([]byte, error) {
-	if len(data)%aes.BlockSize != 0 {
-		return nil, errors.New("The message is not a multiple of the block size")
-	}
+	data = Pad(data)
 
+	fmt.Print("Checkpoint1")
 	block, err := aes.NewCipher(c.key)
 	if err != nil {
+		fmt.Print("Checkpoint2")
 		return nil, err
+		fmt.Print("Checkpoint3")
 	}
 
+	fmt.Print("Checkpoint4")
+
+	selectedMode := c.mode
+	switch selectedMode {
+	case "Galois Counter Mode (GCM)":
+		fmt.Print("GCM")
+	case "Cipher Block Chaining (CBC)":
+		fmt.Print("CBC")
+	case "Cipher Feedback (CFB)":
+		fmt.Print("CFB")
+	case "Counter (CTR)":
+		fmt.Print("CTR")
+	default: 
+		return nil, errors.New("Undefined mode selected")
+	}
 	cipherText := make([]byte, aes.BlockSize+len(data))
 	iv := cipherText[:aes.BlockSize]
 
@@ -29,25 +49,44 @@ func(c *AdvancedEncryptionStandard) Process(data []byte) ([]byte, error) {
 	return cipherText[:], nil
 }
 
+func Pad(data []byte) ([]byte) {
+	lenOfData := len(data)
+	data = append(data, 0)
+	copy(data[1:], data)
+	data[0] = byte(lenOfData)
+
+	for i := 0; i < padIndex; i++ {
+		data = append(data, 0)
+		copy(data[1:], data)
+		data[0] = 0
+	}
+
+	for len(data)%aes.BlockSize != 0 {
+		data = append(data, 0)
+	}
+	
+	return data[:]
+}
+
+func UnPad(data []byte) ([]byte) {
+	lenOfData := data[padIndex]
+	data = data[padIndex+1:]
+	return data[:lenOfData]
+}
+
 func(c *AdvancedEncryptionStandard) Unprocess(data []byte) ([]byte, error) {
 	block, err := aes.NewCipher(c.key)
 	if err != nil {
 		return nil, err
 	}
 
-	if len(data) < aes.BlockSize {
-		return nil, errors.New("The message is too short for given key")
-	}
-
 	iv := data[:aes.BlockSize]
 	data = data[aes.BlockSize:]
 
-	if len(data)%aes.BlockSize != 0 {
-		return nil, errors.New("ciphertext is not a multiple of the block size")
-	}
-
 	mode := cipher.NewCBCDecrypter(block, iv)
 	mode.CryptBlocks(data, data)
+
+	data = UnPad(data)
 
 	return data[:], nil
 } 
--- go_covert_lib/controller/processor/advancedEncryptionStandard/advancedEncryptionStandard_config.go
+++ go_covert_lib/controller/processor/advancedEncryptionStandard/advancedEncryptionStandard_config.go
@@ -5,14 +5,16 @@ import (
 )
 
 type ConfigClient struct {
+	Mode config.SelectParam
 	Key config.HexKeyParam
 }
 
 func GetDefault() ConfigClient {
 	return ConfigClient {
+		Mode: config.MakeSelect("Galois Counter Mode (GCM)", []string{"Galois Counter Mode (GCM)", "Cipher Block Chaining (CBC)", "Cipher Feedback (CFB)", "Counter (CTR)"}, config.Display{Description: "Select the mode of operation."}),
 		Key: config.MakeHexKey(make([]byte, 32), []int{16, 24, 32}, config.Display{Description: "The shared secret key used for symmtric encryption."})}
 } 
 
 func ToProcessor(aes ConfigClient) (*AdvancedEncryptionStandard, error) {
-	return &AdvancedEncryptionStandard{key: aes.Key.Value}, nil
+	return &AdvancedEncryptionStandard{mode: aes.Mode.Value, key: aes.Key.Value}, nil
 }
