f1ca0f1e8a1b650f7afbf98b62a85a32153e3334
Ryan Abraham <Ryan_JA@live.ca>
2020-01-10 16:41:22 -0500

    [22] Add IPInput implementation & loading spinner

--- go_covert_lib/client/src/App.jsx
+++ go_covert_lib/client/src/App.jsx
@@ -2,15 +2,10 @@ import React, { useState, useEffect } from 'react';
 import Button from 'react-bootstrap/Button';
 import Dropdown from 'react-bootstrap/Dropdown';
 import FormControl from 'react-bootstrap/FormControl';
+import Spinner from 'react-bootstrap/Spinner';
 
 import IPInput from './ui-components/IPInput';
 import './styles.css';
-
-// Matches just the "127.0.0.1:8080" portion of the address
-const addressRegex = /[a-zA-Z0-9.]+:[\d]+/g;
-// const ws = new WebSocket(`ws://${window.location.href.match(addressRegex)[0]}/api/ws`);
-// TODO: The below exists for easy personal debugging
-const ws = new WebSocket('ws://localhost:8080/api/ws');
 
 /**
  * IMPORTANT NOTE: For styling, refer to https://getbootstrap.com/docs/4.0/utilities/position/
@@ -21,37 +16,44 @@ const App = () => {
   const [processor, setProcessor] = useState({});
   const [channels, setChannels] = useState([]);
   const [channel, setChannel] = useState({});
+  const [config, setConfig] = useState({});
+  const [isLoading, setLoading] = useState(true);
 
-  const sendConfig = () => {
+  const sendConfig = (ws) => {
     const cmd = JSON.stringify({ OpCode: 'config' });
     ws.send(cmd);
   };
 
+  const handleMessage = (msg) => {
+    switch (msg.OpCode) {
+      case 'config':
+        setChannels(msg.Default.Channel);
+        setProcessors(msg.Default.Processor);
+        setLoading(false);
+        break;
+      default:
+          // TODO:
+    }
+  };
+
   useEffect(() => {
+    // Matches just the "127.0.0.1:8080" portion of the address
+    const addressRegex = /[a-zA-Z0-9.]+:[\d]+/g;
+    const ws = new WebSocket(`ws://${window.location.href.match(addressRegex)[0]}/api/ws`);
+    // TODO: The line below exists for easy personal debugging
+    // const ws = new WebSocket('ws://localhost:8080/api/ws');
     ws.binaryType = 'arraybuffer';
-    ws.onopen = (event) => {
-      sendConfig();
-    };
-
-    ws.onerror = (event) => {
-      // TODO:
-    };
-
-    ws.onmessage = (event) => {
-      const msg = JSON.parse(event.data);
-      switch (msg.OpCode) {
-        case 'config':
-          setChannels(msg.Default.Channel);
-          setProcessors(msg.Default.Processor);
-          break;
-        default:
-            // TODO:
-      }
-    };
+    ws.onopen = _e => sendConfig(ws);
+    ws.onerror = _e => console.log('UNIMPLEMENTED'); //TODO:
+    ws.onmessage = e => handleMessage(JSON.parse(e.data));
   }, []);
 
-  return (
-    <div className="App m-2">
+  return isLoading ? (
+    <div className="spinner-container">
+      <Spinner animation="border" role="status" />
+    </div>
+  ) : (
+    <div className="m-2">
       <h2 className="m-1">Messaging</h2>
       <FormControl
         as="textarea"
@@ -75,7 +77,6 @@ const App = () => {
         >
           {channel.value || 'Select a Channel'}
         </Dropdown.Toggle>
-
         <Dropdown.Menu className="w-25">
           {
             Object.keys(channels).map(chan => (
--- go_covert_lib/client/src/ui-components/IPInput.jsx
+++ go_covert_lib/client/src/ui-components/IPInput.jsx
@@ -1,14 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import FormControl from 'react-bootstrap/FormControl';
+import InputGroup from 'react-bootstrap/InputGroup';
+
 
 const IPInput = (props) => {
+  const { label } = props;
   return (
-    <div>
-    </div>
+    <InputGroup className="m-1 w-25">
+      <InputGroup.Prepend>
+        <InputGroup.Text className="input-text">{label}</InputGroup.Text>
+      </InputGroup.Prepend>
+      <FormControl
+        placeholder="127.0.0.1"
+      />
+    </InputGroup>
   );
 };
 
 IPInput.propTypes = {
+  label: PropTypes.string.isRequired,
 };
 
 export default IPInput;
