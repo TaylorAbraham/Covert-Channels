59650375f559cd95ff6a00591a41ed6bd2e0f4ae
jacksonschoe <jacksonschoenermarck@cmail.carleton.ca>
2020-02-21 19:31:05 -0500

    http normal, cant get tests to reliably work

---
+++ go_covert_lib/controller/channel/httpNormal/httpNormal.go
@@ -0,0 +1,177 @@
+package httpNormal
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"io"
+	"log"
+	"net"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+const maxMsg = 32
+
+// This is a normal, non-covert tcp messaging channel
+// The message is sent using normal TCP packets with the proper OS tcp functions
+type Config struct {
+	FriendIP   [4]byte
+	OriginIP   [4]byte
+	FriendPort uint16
+	OriginPort uint16
+	UserType   bool
+
+	ReadTimeout  time.Duration
+	WriteTimeout time.Duration
+}
+
+// A TCP covert channel
+type Channel struct {
+	conf Config
+	srv  *http.Server
+
+	cancel chan bool
+
+	serverSendBuf chan []byte
+	serverRecBuf  chan []byte
+}
+
+// Create the covert channel, filling in the SeqEncoder
+// with a default if none is provided
+// Although the error is not yet used, it is anticipated
+// that this function may one day be used for validating
+// the data structure
+func MakeChannel(conf Config) (*Channel, error) {
+	c := &Channel{conf: conf, cancel: make(chan bool)}
+
+	if conf.UserType {
+		mux := http.NewServeMux()
+		mux.HandleFunc("/", c.handleFunc)
+
+		c.srv = &http.Server{Addr: ":" + strconv.Itoa(int(conf.OriginPort)), Handler: mux}
+		go func() { c.srv.ListenAndServe() }()
+
+		c.serverSendBuf = make(chan []byte, maxMsg)
+		c.serverRecBuf = make(chan []byte, maxMsg)
+	}
+	return c, nil
+}
+
+func (c *Channel) handleFunc(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		select {
+		case data := <-c.serverSendBuf:
+			io.WriteString(w, string(data))
+		case <-time.After(time.Millisecond * 50):
+			log.Println("GET Request timeout")
+		case <-c.cancel:
+		}
+	} else if r.Method == "POST" {
+		buf := bytes.Buffer{}
+		buf.ReadFrom(r.Body)
+		select {
+		case c.serverRecBuf <- buf.Bytes():
+		case <-time.After(time.Millisecond * 50):
+			log.Println("GET Request timeout")
+		case <-c.cancel:
+		}
+	} else {
+		log.Println("Invalid Covert Message")
+	}
+}
+
+func (c *Channel) Close() error {
+	select {
+	case <-c.cancel:
+		return nil
+	default:
+		close(c.cancel)
+		if c.conf.UserType {
+			ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+			defer cancel()
+			return c.srv.Shutdown(ctx)
+		}
+	}
+	return nil
+}
+
+func (c *Channel) Receive(data []byte) (uint64, error) {
+	if c.conf.UserType {
+		if c.conf.ReadTimeout == 0 {
+			select {
+			case httpdata := <-c.serverRecBuf:
+				copy(data, httpdata)
+				if len(httpdata) > len(data) {
+					return uint64(len(data)), errors.New("Buffer overflow")
+				} else {
+					return uint64(len(httpdata)), nil
+				}
+			case <-c.cancel:
+				return 0, errors.New("Channel closed")
+			}
+		} else {
+			select {
+			case httpdata := <-c.serverRecBuf:
+				copy(data, httpdata)
+				if len(httpdata) > len(data) {
+					return uint64(len(data)), errors.New("Buffer overflow")
+				} else {
+					return uint64(len(httpdata)), nil
+				}
+			case <-time.After(c.conf.WriteTimeout):
+				return 0, errors.New("Write Timeout")
+			case <-c.cancel:
+				return 0, errors.New("Channel closed")
+			}
+		}
+	} else {
+		addr := &net.TCPAddr{IP: c.conf.FriendIP[:], Port: int(c.conf.FriendPort)}
+		resp, err := http.Get("http://" + addr.String() + "/")
+		log.Println("GET")
+		if err == nil {
+			buf := bytes.Buffer{}
+			buf.ReadFrom(resp.Body)
+			copy(data, buf.Bytes())
+			if len(buf.Bytes()) > len(data) {
+				return uint64(len(data)), errors.New("Buffer overflow")
+			} else {
+				return uint64(len(buf.Bytes())), nil
+			}
+		} else {
+			return 0, err
+		}
+	}
+}
+
+func (c *Channel) Send(data []byte) (uint64, error) {
+	if c.conf.UserType {
+		if c.conf.WriteTimeout == 0 {
+			select {
+			case c.serverSendBuf <- data:
+				return uint64(len(data)), nil
+			case <-c.cancel:
+				return 0, errors.New("Channel closed")
+			}
+		} else {
+			select {
+			case c.serverSendBuf <- data:
+				return uint64(len(data)), nil
+			case <-time.After(c.conf.WriteTimeout):
+				return 0, errors.New("Write Timeout")
+			case <-c.cancel:
+				return 0, errors.New("Channel closed")
+			}
+		}
+	} else {
+		addr := &net.TCPAddr{IP: c.conf.FriendIP[:], Port: int(c.conf.FriendPort)}
+		_, err := http.Post("http://"+addr.String()+"/", "text/plain", bytes.NewBuffer(data))
+		log.Println("POST")
+		if err == nil {
+			return uint64(len(data)), nil
+		} else {
+			return 0, err
+		}
+	}
+}
---
+++ go_covert_lib/controller/channel/httpNormal/httpNormal_test.go
@@ -0,0 +1,151 @@
+package httpNormal
+
+import (
+	"bytes"
+	"log"
+	"testing"
+	"time"
+)
+
+var sconf Config = Config{
+	FriendIP:   [4]byte{127, 0, 0, 1},
+	OriginIP:   [4]byte{127, 0, 0, 1},
+	FriendPort: 2001,
+	OriginPort: 2000,
+	UserType:   true,
+}
+
+var rconf Config = Config{
+	FriendIP:   [4]byte{127, 0, 0, 1},
+	OriginIP:   [4]byte{127, 0, 0, 1},
+	FriendPort: 2000,
+	OriginPort: 2001,
+	UserType:   false,
+}
+
+func TestReceiveSendGet(t *testing.T) {
+
+	log.Println("Starting TestReceiveSend")
+
+	sch, err := MakeChannel(sconf)
+	if err != nil {
+		t.Errorf("err = '%s'; want nil", err.Error())
+	}
+
+	rch, err := MakeChannel(rconf)
+	if err != nil {
+		t.Errorf("err = '%s'; want nil", err.Error())
+	}
+
+	var (
+		c    chan []byte = make(chan []byte)
+		rErr error
+		nr   uint64
+		// Test with message with many characters and with 0 characters
+		inputs [][]byte = [][]byte{[]byte("Hello world!"), []byte("")}
+	)
+
+	for _, input := range inputs {
+		go func() {
+			var data [15]byte
+			nr, rErr = rch.Receive(data[:])
+			select {
+			case c <- data[:nr]:
+			case <-time.After(time.Second * 5):
+			}
+		}()
+
+		sendAndCheck(t, input, sch)
+
+		receiveAndCheck(t, input, c)
+
+		if rErr != nil {
+			t.Errorf("err = '%s'; want nil", rErr.Error())
+		}
+	}
+	if err := sch.Close(); err != nil {
+		t.Errorf("err = '%s'; want nil", err.Error())
+	}
+	if err := rch.Close(); err != nil {
+		t.Errorf("err = '%s'; want nil", err.Error())
+	}
+}
+
+func TestReceiveSendPost(t *testing.T) {
+
+	log.Println("Starting TestReceiveSend")
+
+	sconfcopy := sconf
+	sconfcopy.FriendPort = 3001
+	sconfcopy.OriginPort = 3002
+	sconfcopy.UserType = false
+
+	sch, err := MakeChannel(sconfcopy)
+	if err != nil {
+		t.Errorf("err = '%s'; want nil", err.Error())
+	}
+
+	rconfcopy := rconf
+	rconfcopy.FriendPort = 3002
+	rconfcopy.OriginPort = 3001
+	rconfcopy.UserType = true
+
+	rch, err := MakeChannel(rconfcopy)
+	if err != nil {
+		t.Errorf("err = '%s'; want nil", err.Error())
+	}
+
+	var (
+		c    chan []byte = make(chan []byte)
+		rErr error
+		nr   uint64
+		// Test with message with many characters and with 0 characters
+		inputs [][]byte = [][]byte{[]byte("Hello world!"), []byte("")}
+	)
+
+	for _, input := range inputs {
+		go func() {
+			var data [15]byte
+			nr, rErr = rch.Receive(data[:])
+			select {
+			case c <- data[:nr]:
+			case <-time.After(time.Second * 5):
+			}
+		}()
+
+		sendAndCheck(t, input, sch)
+
+		receiveAndCheck(t, input, c)
+
+		if rErr != nil {
+			t.Errorf("err = '%s'; want nil", rErr.Error())
+		}
+	}
+	if err := sch.Close(); err != nil {
+		t.Errorf("err = '%s'; want nil", err.Error())
+	}
+	if err := rch.Close(); err != nil {
+		t.Errorf("err = '%s'; want nil", err.Error())
+	}
+}
+
+func sendAndCheck(t *testing.T, input []byte, sch *Channel) {
+	n, err := sch.Send(input)
+	if err != nil {
+		t.Errorf("err = '%s'; want nil", err.Error())
+	}
+	if n != uint64(len(input)) {
+		t.Errorf("send n = %d; want %d", n, len(input))
+	}
+}
+
+func receiveAndCheck(t *testing.T, input []byte, c chan []byte) {
+	select {
+	case received := <-c:
+		if bytes.Compare(received, input) != 0 {
+			t.Errorf("received = %s; want %s", string(received), string(input))
+		}
+	case <-time.After(time.Second * 5):
+		t.Errorf("Read timeout")
+	}
+}
