ee2664e4af8b818c9106ad4179be4eb2e7d6e252
Fahid-Mannan <fahidmannan@cmail.carleton.ca>
2020-03-24 19:09:06 -0400

    icmp covert channel configuration files'

---
+++ go_covert_lib/controller/channel/icmpIP/embedders.go
@@ -0,0 +1,41 @@
+package icmpIP
+
+import (
+	"../embedders"
+	"errors"
+	"github.com/google/gopacket/layers"
+	"golang.org/x/net/ipv4"
+)
+
+type IcmpEncoder interface {
+	GetByte(ipv4h ipv4.Header, icmph layers.ICMP, maskIndex int) ([]byte, error)
+	SetByte(ipv4h ipv4.Header, icmph layers.ICMP, buf []byte, maskIndex int) (ipv4.Header, layers.ICMP, []byte, error)
+	GetMask() [][]byte
+}
+
+// Encoder stores one byte per packet in the lowest order byte of the IPV4 header ID
+type IDEncoder struct {
+	emb *embedders.IDEncoder
+}
+
+func (e *IDEncoder) GetByte(ipv4h ipv4.Header, icmph layers.ICMP, maskIndex int) ([]byte, error) {
+	if b, err := e.emb.GetByte(ipv4h); err == nil {
+		return []byte{b}, nil
+	} else {
+		return nil, err
+	}
+}
+func (e *IDEncoder) SetByte(ipv4h ipv4.Header, icmph layers.ICMP, buf []byte, maskIndex int) (ipv4.Header, layers.ICMP, []byte, error) {
+	if len(buf) == 0 {
+		return ipv4h, icmph, nil, errors.New("Cannot set byte if no data")
+	}
+	if newipv4h, err := e.emb.SetByte(ipv4h, buf[0]); err == nil {
+		return newipv4h, icmph, buf[1:], nil
+	} else {
+		return ipv4h, icmph, buf, err
+	}
+}
+
+func (s *IDEncoder) GetMask() [][]byte {
+	return [][]byte{[]byte{0xFF}}
+}
